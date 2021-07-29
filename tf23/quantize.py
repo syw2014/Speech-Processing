27eba6024b4117c40736347e8cc12ae559eea1dc
syw2014 <jerryshi0110@gmail.com>
2021-07-29 11:38:12 +0800

    optimizated convert model to tflite, do not use optimizations

--- tf23/quantize.py
+++ tf23/quantize.py
@@ -18,6 +18,7 @@ import data_process
 import models
 from test_tflite import tflite_test
 
+tf.compat.v1.enable_eager_execution()
 NUM_REP_DATA_SAMPLES = 100  # How many samples to use for post training quantization.
 
 
@@ -68,12 +69,12 @@ def post_training_quantize(keras_model, rep_dataset):
         Quantized TFLite model ready for saving to disk.
     """
     converter = tf.lite.TFLiteConverter.from_keras_model(keras_model)
-    converter.optimizations = [tf.lite.Optimize.DEFAULT]
+    #converter.optimizations = [tf.lite.Optimize.DEFAULT]
 
     # Int8 post training quantization needs representative dataset.
-    converter.representative_dataset = rep_dataset
-    converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
-
+    # converter.representative_dataset = rep_dataset
+    #converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+    converter.post_training_quantize = True
     tflite_model = converter.convert()
 
     return tflite_model
@@ -83,7 +84,7 @@ def main():
     model_settings = models.prepare_model_settings(
         len(data_process.prepare_words_list(FLAGS.wanted_words.split(','))),
         FLAGS.sample_rate, FLAGS.clip_duration_ms, FLAGS.window_size_ms,
-        FLAGS.window_stride_ms, FLAGS.dct_coefficient_count)
+        FLAGS.window_strides_ms, FLAGS.dct_coefficient_count)
 
     audio_processor = data_process.AudioProcessor(data_dir=FLAGS.data_dir,
                                                   silence_percentage=FLAGS.silence_percentage,
@@ -103,87 +104,109 @@ def main():
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '--data_url',
-        type=str,
-        default='http://download.tensorflow.org/data/speech_commands_v0.02.tar.gz',
-        help='Location of speech training data archive on the web.')
-    parser.add_argument(
-        '--data_dir',
-        type=str,
-        default='/tmp/speech_dataset/',
-        help="""\
-        Where to download the speech training data to.
-        """)
-    parser.add_argument(
-        '--silence_percentage',
-        type=float,
-        default=10.0,
-        help="""\
-        How much of the training data should be silence.
-        """)
-    parser.add_argument(
-        '--unknown_percentage',
-        type=float,
-        default=10.0,
-        help="""\
-        How much of the training data should be unknown words.
-        """)
-    parser.add_argument(
-        '--testing_percentage',
-        type=int,
-        default=10,
-        help='What percentage of wavs to use as a test set.')
-    parser.add_argument(
-        '--validation_percentage',
-        type=int,
-        default=10,
-        help='What percentage of wavs to use as a validation set.')
-    parser.add_argument(
-        '--sample_rate',
-        type=int,
-        default=16000,
-        help='Expected sample rate of the wavs', )
-    parser.add_argument(
-        '--clip_duration_ms',
-        type=int,
-        default=1000,
-        help='Expected duration in milliseconds of the wavs', )
-    parser.add_argument(
-        '--window_size_ms',
-        type=float,
-        default=30.0,
-        help='How long each spectrogram timeslice is', )
-    parser.add_argument(
-        '--window_stride_ms',
-        type=float,
-        default=10.0,
-        help='How long each spectrogram timeslice is', )
-    parser.add_argument(
-        '--dct_coefficient_count',
-        type=int,
-        default=40,
-        help='How many bins to use for the MFCC fingerprint', )
-    parser.add_argument(
-        '--wanted_words',
-        type=str,
-        default='yes,no,up,down,left,right,on,off,stop,go',
-        help='Words to use (others will be added to an unknown label)', )
-    parser.add_argument(
-        '--model_architecture',
-        type=str,
-        default='dnn',
-        help='What model architecture to use')
-    parser.add_argument(
-        '--model_size_info',
-        type=int,
-        nargs="+",
-        default=[128, 128, 128],
-        help='Model dimensions - different for various models')
-    parser.add_argument(
-        '--checkpoint',
-        type=str,
-        help='Checkpoint to load the weights from.')
 
+    # *NOTE*, here we copy the parameter from train directly, there are some parameters may not used
+    parser.add_argument("--data_dir",
+                        type=str,
+                        default="/data1/yw.shi/data/audio/",
+                        help="directory of audio wav file")
+    parser.add_argument("--background_volume",
+                        type=float,
+                        default=0.1,
+                        help="how loud the background noise should be , between 0~1")
+    parser.add_argument("--background_frequency",
+                        type=float,
+                        default=0.0,
+                        help="how many of the training samples have background noise mixed in")
+
+    parser.add_argument("--silence_percentage",
+                        type=float,
+                        default=30.0,
+                        help="how many of the training data should be silence")
+    parser.add_argument("--unknown_percentage",
+                        type=float,
+                        default=100.0,
+                        help="how much of the training data should be unknown words")
+    parser.add_argument("--augment_percentage",
+                        default=30.0,
+                        help="how much of the training data should be augment")
+    parser.add_argument("--time_shift_ms",
+                        type=float,
+                        default=100.0,
+                        help="range to randomly shift the training audio by in time")
+
+    parser.add_argument("--sample_rate",
+                        type=int,
+                        default=16000,
+                        help="expected sample rate of the wavs")
+    parser.add_argument("--clip_duration_ms",
+                        type=int,
+                        default=1000,
+                        help="expected duration in milliseconds of the wavs")
+    parser.add_argument("--window_size_ms",
+                        type=float,
+                        default=30.0,
+                        help="how long each spectrogram timeslice is")
+    parser.add_argument("--window_strides_ms",
+                        type=float,
+                        default=10.0,
+                        help="how long each spectrogram timeslice is")
+    parser.add_argument("--dct_coefficient_count",
+                        type=int,
+                        default=40,
+                        help="how many bins to use for the mfcc fingerprint")
+    parser.add_argument("--how_many_train_steps",
+                        type=str,
+                        default="10000,10000, 10000",
+                        help="how many training loops to run")
+    parser.add_argument("--eval_step_interval",
+                        type=int,
+                        default=400,
+                        help="how often to evaluate the training results")
+    parser.add_argument("--learning_rate",
+                        type=str,
+                        default="0.0001, 0.0001, 0.00002",
+                        help="how large a learning rate to use when training.")
+    parser.add_argument("--batch_size",
+                        type=int,
+                        default=256,
+                        help="how many items to train with at once")
+    parser.add_argument("--summaries_dir",
+                        type=str,
+                        default="./result/train_summary",
+                        help="summary directory")
+    parser.add_argument("--wanted_words",
+                        type=str,
+                        default="nihaoxiaoshun,xiaoshunxiaoshun",
+                        help="keywords list")
+    parser.add_argument("--train_dir",
+                        type=str,
+                        default="./result/models",
+                        help="train folder")
+    parser.add_argument("--checkpoint",
+                        type=str,
+                        default="./result/models/best/ds_cnn_0.988_ckpt",
+                        help="checkpoint folder")
+    parser.add_argument("--start_checkpoint",
+                        type=str,
+                        default="",
+                        help="restore pre-trained model before any training")
+    parser.add_argument("--model_architecture",
+                        type=str,
+                        default="ds_cnn",
+                        help="what model to use dnn/cnn/ds_cnn")
+    parser.add_argument("--model_size_info",
+                        type=int,
+                        nargs="+",
+                        default=[6, 276, 10, 4, 2, 1, 276, 3, 3, 2, 2, 276, 3, 3, 1, 1, 276, 3, 3, 1, 1, 276, 3, 3, 1,
+                                 1, 276, 3, 3, 1, 1],
+                        # default=[64, 20, 8, 1, 1, 64, 10, 4, 1, 1, 64, 10, 4, 1, 1, 256, 256],
+                        help="model parameters specified with different model")
+    parser.add_argument("--mlir_file",
+                        type=str,
+                        default="./cnn.mlir",
+                        help="model parameters specified with different model")
+
+    #    parser = argparse.ArgumentParser()
     FLAGS, _ = parser.parse_known_args()
     main()
