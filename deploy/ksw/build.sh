3988da771eee134b5da6598b4ad6ba67d4bb860b
syw2014 <jerryshi0110@gmail.com>
2021-07-29 13:51:34 +0800

    demo compile command

---
+++ deploy/ksw/build.sh
@@ -0,0 +1,10 @@
+###
+ # @Author: your name
+ # @Date: 2021-07-29 13:48:23
+ # @LastEditTime: 2021-07-29 13:51:04
+ # @LastEditors: Please set LastEditors
+ # @Description: In User Settings Edit
+ # @FilePath: \deploy\ksw\build.sh
+### 
+
+g++ -std=c++ demo.cc -o demo mfcc.cc -I ./
